55b24982482243f04751107806cde12b391a41d0
Emre Arabaci <emrearabaci16@gmail.com>
2024-08-11 01:36:14 +0300

    Created global config file.

---
+++ src/app/_configs/configs.ts
@@ -0,0 +1,31 @@
+/* Icon Attributes */
+export const imageConfigs = {
+  width: 18,
+  height: 18,
+  quality: 100,
+  priority: true
+};
+
+/* Icon Set */
+export const icons = {
+  circleHalf: {
+    dark: '/icon-circle-half-light.svg',
+    light: '/icon-circle-half-dark.svg'
+  },
+  pieChart: {
+    dark: '/icon-pie-chart-light.svg',
+    light: '/icon-pie-chart-dark.svg'
+  },
+  search: {
+    dark: '/icon-search-light.svg',
+    light: '/icon-search-dark.svg'
+  },
+  dot: {
+    dark: '/icon-dot-light.svg',
+    light: '/icon-dot-dark.svg'
+  },
+  activity: {
+    dark: '/icon-activity-light.svg',
+    light: '/icon-activity-dark.svg'
+  }
+};
