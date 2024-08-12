d83617e841024766c60f227249dd477e9636bbeb
Emre Arabaci <emrearabaci16@gmail.com>
2024-08-13 01:00:28 +0300

    Added "Navigation" component to root layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from 'next';
 /* Context */
 import { ThemeProvider } from '@/app/_context/ThemeContext';
 
+/* Components */
+import Navigation from '@/app/_components/Navigation/Navigation';
+
 /* Fonts */
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
@@ -24,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <Navigation />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
